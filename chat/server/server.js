49903333aae3edd5cc55b17b72a4b7e8e9fc460d
Wahab Noor <104102500+wahab3913@users.noreply.github.com>
2022-12-23 23:15:55 +0500

    delete

--- chat/server/server.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const app = require("./app");
-
-const port = process.env.PORT || 4000;
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Server is running on port ${port}`));
