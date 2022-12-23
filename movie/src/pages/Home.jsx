fd9cf47b26b8f43854f64959aa2eb3afdcaa18da
Wahab Noor <wahabnoor315@gmail.com>
2022-12-23 23:12:13 +0500

    chat app

---
+++ movie/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const pages = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+];
+
+const Home = () => {
+  return <></>;
+};
+
+export default Home;
